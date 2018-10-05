1162eec80d70e06d31c3adec9d3443105c587e7f
Marcus Peng <ms855187@gmail.com>
2018-10-06 02:26:42 +0800

    Move class to new file

--- Otp/AuthenticationService.cs
+++ Otp/AuthenticationService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Otp.Daos;
 
 namespace Otp
 {
@@ -13,57 +12,11 @@ namespace Otp
 
             // 根據 account 取得 RSA token 目前的亂數
             var rsaToken = new RsaTokenDao();
-            var randomCode = rsaToken.GetRandom(account);
+            var randomCode = rsaToken.GetRandom();
             
             // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
             var validPassword = passwordFromDao + randomCode;
-            var isValid = passcode == validPassword;
-
-            if (isValid)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    
-    public class ProfileDao
-    {
-        public string GetPassword(string account)
-        {
-            return Context.GetPassword(account);
-        }
-    }
-
-    public static class Context
-    {
-        public static Dictionary<string, string> profiles;
-
-        static Context()
-        {
-            profiles = new Dictionary<string, string>();
-            profiles.Add("joey", "91");
-            profiles.Add("mei", "99");
-        }
-
-        public static string GetPassword(string key)
-        {
-            return profiles[key];
-        }
-    }
-
-    public class RsaTokenDao
-    {
-        public string GetRandom(string account)
-        {
-            var seed = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
-            var result = seed.Next(0, 999999).ToString("000000");
-            Console.WriteLine("randomCode:{0}", result);
-
-            return result;
+            return passcode == validPassword;
         }
     }
 }
