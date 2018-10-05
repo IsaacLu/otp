04ba7f1626b77673dd285bc56aa024d67a8f391d
Marcus Peng <ms855187@gmail.com>
2018-10-06 01:47:15 +0800

    Initial

---
+++ OtpTests/AuthenticationServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Otp;
+
+namespace OtpTests
+{
+    [TestFixture]
+    public class AuthenticationServiceTests
+    {
+        [Test]
+        public void IsValidTest()
+        {
+            var target = new AuthenticationService();
+
+            var actual = target.IsValid("joey", "91000000");
+            Assert.True(actual);
+        }
+    }
+}
